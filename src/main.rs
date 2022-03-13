5a50bd37404266d0b5b656d125b1d9bcf3a2cf86
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-03-13 21:36:29 +0100

    non multi

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use hyper_tls::HttpsConnector;
 use std::iter;
 use std::{sync::Arc, time::Instant};
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     start().await
 }
@@ -13,35 +13,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn start() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://ascella.wtf/v2/ascella/view/zBNwf9q";
     let times = 500;
-    let threads = 50;
     let started = Instant::now();
-    // let https = HttpsConnector::new();
-    // let client = Arc::new(Client::builder().build::<_, hyper::Body>(https));
+    let https = HttpsConnector::new();
+    let client = Arc::new(Client::builder().build::<_, hyper::Body>(https));
 
-    let r = future::join_all(iter::repeat(0).take(threads).map(|thread| {
-        // let times = times;
-        tokio::spawn(async move {
-            let https = HttpsConnector::new();
-            let client_arc = Arc::new(Client::builder().build::<_, hyper::Body>(https));
-            let client = client_arc.clone();
-            async move {
-                future::join_all(iter::repeat(0).take(times / threads).map(|_| {
-                    let client = client.clone();
-                    async move {
-                        println!("Starting req thread {thread}");
-                        let r = &client.get(url.parse().unwrap()).await.unwrap();
-                        println!("Ending req {}", r.status());
-                    }
-                }))
-                .await;
-            }
-        })
+    future::join_all(iter::repeat(0).take(times).map(|_| {
+        let client = client.clone();
+        async move {
+            println!("Starting req");
+            let r = &client.get(url.parse().unwrap()).await.unwrap();
+            println!("Ending req {}", r.status());
+        }
     }))
     .await;
-    for handle in r {
-        handle.unwrap().await;
-    }
-
     let now = Instant::now();
     let elapsed = now.duration_since(started);
     println!(
